1f4009ab9eac4676c3b9817fd272012cacd01bb6
DormBug <dormbug@gmail.com>
2020-01-28 17:39:42 +0200

    AAdded init sql scripts for creating tables and default data by using Liquibase

---
+++ orke/db-migration/src/main/resources/db/init/init_data.sql
@@ -0,0 +1,7 @@
+INSERT IGNORE INTO `colony` (`Id`, `Name`, `Population_Count`) VALUES (1, 'Moscow', 3450);
+INSERT IGNORE INTO `colony` (`Id`, `Name`, `Population_Count`) VALUES (2, 'Beliz', 123);
+INSERT IGNORE INTO `colony` (`Id`, `Name`, `Population_Count`) VALUES (3, 'Kiev', 1200);
+
+INSERT IGNORE INTO `state` (`Id`, `Name`, `Text`) VALUES (1, 'Hello Page', 'Hi everyone!');
+INSERT IGNORE INTO `state` (`Id`, `Name`, `Text`) VALUES (2, 'Bye Page', 'Bye everyone, see you later!');
+INSERT IGNORE INTO `state` (`Id`, `Name`, `Text`) VALUES (3, 'Joke Page', 'I have no joke in Engling, sorry(');
